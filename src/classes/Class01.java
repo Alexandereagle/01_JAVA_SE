0be4f672f77785e5b725b865eb73c6f1175847dc
zhangzhen <1625855092@qq.com>
2020-12-17 16:51:55 +0800

    JAVA的第一节课和第二节课

--- src/classes/Class01.java
+++ src/classes/Class01.java
@@ -2,6 +2,29 @@ package classes;
 
 public class Class01 {
     public static void main(String[] args) {
+        printString();
+        printString("iPhone");
+        printString("first", "second", "third", "last one");
+        int a = 10;
+        if (a == 10)
+            a = 12;
+    }
+
+    // 编译: javac Class01.java
+    // 运行: java Class01
+    static void printString(){
         System.out.println("Hello World");
+    }
+
+    static void printString(String firstStr){
+        System.out.println("Hello " + firstStr);
+    }
+
+    static void printString(String first, String ...args){
+        System.out.println(first);
+        for (int i = 0; i < args.length; i++){
+            System.out.print(args[i]);
+            System.out.print(" ");
+        }
     }
 }
--- src/working/Work01.java
+++ src/working/Work01.java
@@ -1,4 +1,28 @@
 package working;
 
-public class Work01 {
+class Work01 {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Work01HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+class Work01BaseInformation {
+    public static void main(String[] args) {
+        System.out.println("张震");
+        System.out.println("22");
+        System.out.println("男");
+        System.out.println("北京市 昌平区");
+    }
 }
