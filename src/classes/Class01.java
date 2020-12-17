76315e71ed580b0bfdf208b147d7416ee16ed43c
zhangzhen <1625855092@qq.com>
2020-12-17 16:53:10 +0800

    JAVA的第一节课和第二节课

--- src/classes/Class01.java
+++ src/classes/Class01.java
@@ -12,17 +12,17 @@ public class Class01 {
 
     // 编译: javac Class01.java
     // 运行: java Class01
-    static void printString(){
+    static void printString() {
         System.out.println("Hello World");
     }
 
-    static void printString(String firstStr){
+    static void printString(String firstStr) {
         System.out.println("Hello " + firstStr);
     }
 
-    static void printString(String first, String ...args){
+    static void printString(String first, String... args) {
         System.out.println(first);
-        for (int i = 0; i < args.length; i++){
+        for (int i = 0; i < args.length; i++) {
             System.out.print(args[i]);
             System.out.print(" ");
         }
