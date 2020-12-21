0ebc58236951f46d8bd9b33c1fdd8238bad4101d
zhangzhen <1625855092@qq.com>
2020-12-21 16:53:07 +0800

    提交homework4

--- src/classes/Class05.java
+++ src/classes/Class05.java
@@ -90,5 +90,148 @@ class PrintStars {
             }
             System.out.println();
         }
+
+        int a = 10;
+        int b = 20;
+        max(a, b);
+
+    }
+
+    public static int max(int a, int b) {
+
+        return a > b ? a : b;
+    }
+}
+
+
+class Student {
+    String name;
+    char gender;
+    int age;
+    int id;
+    double score;
+
+    String say() {
+        return "姓名: " + name + "性别: " + name + " 年龄: " + age + " id: " + id + " 成绩: " + score;
+    }
+}
+
+class TestStudent {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "张三";
+        student.gender = '男';
+        student.age = 18;
+        student.id = 1;
+        student.score = 100.9;
+        System.out.println(student.say());
+        System.out.println(student.name);
+        System.out.println(student.gender);
+        System.out.println(student.age);
+        System.out.println(student.id);
+        System.out.println(student.score);
+    }
+}
+
+class Dog {
+    String name;
+    int age = 1;
+    double weight = 0;
+
+    String say() {
+        return "name:" + name + " age:" + age + " weight:" + weight;
+    }
+}
+
+class TestDog {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        System.out.println(dog.say());
+        Dog dog2 = new Dog();
+        System.out.println(dog2.say());
+
+        dog.name = "京巴";
+        dog2.name = "吉娃娃";
+        dog2.weight = 8;
+        System.out.println(dog.say());
+        System.out.println(dog2.say());
+    }
+}
+
+class Num1 {
+    public int a;
+    public int b;
+
+    public String say() {
+        return "a: " + a + ", b: " + b;
+    }
+}
+
+class TestNum1 {
+    public static void main(String[] args) {
+        Num1 n1 = new Num1();
+        System.out.println("调用函数执行前的值" + n1.say());
+        add(n1);
+        System.out.println("调用函数执行后的值" + n1.say());
+
+    }
+
+    public static void add(Num1 n1) {
+        System.out.println("函数执行前的值" + n1.say());
+        n1.a += 1;
+        n1.b += 2;
+        System.out.println("函数执行后的值" + n1.say());
+    }
+}
+
+class Circle {
+    private final double PI = 3.14159265357939788292;
+    public double radius;
+
+    public double findArea() {
+        return radius * radius * PI;
+    }
+}
+
+class PassObject {
+    public static void printAreas(Circle c, int times) {
+        System.out.println("Radius" + "\t" + "Area");
+        for (int i = 1; i <= times; i++) {
+            c.radius = i;
+            System.out.println(i + "\t\t" + c.findArea());
+        }
+    }
+
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        printAreas(c, 5);
+    }
+}
+
+class Persion {
+    private int age;
+    int b = 10;
+    protected int c = 20;
+    private int d = 30;
+
+    public void setAge(int i) {
+        this.age = i;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+class TestPersion<T> {
+    public static void main(String[] args) {
+        Persion persion = new Persion();
+        persion.b = 10;
+        persion.setAge(18);
+        System.out.println(persion.getAge());
+    }
+
+    public T add(T left, T right) {
+        return left;
     }
 }
---
+++ src/classes/MyAnnotation.java
@@ -0,0 +1,5 @@
+package classes;
+
+public @interface MyAnnotation {
+    String name() default "Hello World";
+}
